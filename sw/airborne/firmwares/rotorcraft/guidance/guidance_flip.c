a5c863c7a74eed3a8c7ec2e59d744e9bf622c9c3
Karan Vir Bains <karan.bains@hotmail.com>
2019-10-18 14:34:48 +0000

    changed the pulse-width

--- sw/airborne/firmwares/rotorcraft/guidance/guidance_flip.c
+++ sw/airborne/firmwares/rotorcraft/guidance/guidance_flip.c
@@ -177,7 +177,7 @@
 #define FIRST_THRUST_DURATION 0.0
 #define STRAIGHT_FLIGHT_DURATION 1.0
 #define THROTTLE_FACTOR 1.0 // 1.0 for hover, increase for increasing altitude
-#define PULSE_DURATION_1 0.1
+#define PULSE_DURATION_1 0.042
 #define FINAL_THRUST_LEVEL 6500
 #define FINAL_THRUST_DURATION 0
 #define ROLL_CMD_NOMINAL 0 //-MAX_PPRZ*30/60 // angle of 30 degrees
